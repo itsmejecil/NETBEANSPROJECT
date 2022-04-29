22b35df4e4faaaff26d19b3db72fac6e7f74761a
Student Admin <student.admin@pn.local>
2022-04-29 19:08:37 +0800

    push

---
+++ NetBeansProjects/Jecilstore/src/jecilstore/Jecilstore.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jecilstore;
+
+/**
+ *
+ * @author student.admin
+ */
+public class Jecilstore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ NetBeansProjects/loginregister1/src/loginregister1/Loginregister1.java
@@ -0,0 +1,11 @@
+
+package loginregister1;
+
+public class Loginregister1 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
