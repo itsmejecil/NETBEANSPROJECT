30fef328d7e4ed3f14f5c294f8f6993d806eb48f
Student Admin <student.admin@pn.local>
2022-05-10 15:37:49 +0800

    save

---
+++ NetBeansProjects/Jecilminimart/src/jecilminimart/Jecilminimart.java
@@ -0,0 +1,14 @@
+
+package jecilminimart;
+
+
+public class Jecilminimart {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Login e = new Login();
+          e.setVisible(true);
+     
+    }
+    
+}
